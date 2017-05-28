47a6b9a8c7067e7163db5a8a4dedec42ec3206ef
yysaki <yysaki@yysaki.com>
2017-05-29 00:37:34 +0900

    Add substitute.lua

---
+++ substitute.lua
@@ -0,0 +1,15 @@
+pastmsg = ''
+
+robot:hear(".+", function(res)
+  if not string.match(res.message.text, "^s/%w+/+%w+/$") then
+    pastmsg = res.message.text
+  else
+    before, after = string.match(res.message.text, "^s/(%w+)/(%w+)/$")
+
+    if not (string.match(pastmsg, before) == nil) then
+      res:send(string.gsub(pastmsg, before, after))
+    else
+      res:send("「" .. before .. "」は見つかりません")
+    end
+  end
+end)
